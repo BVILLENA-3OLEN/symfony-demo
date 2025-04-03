700eef8efa0713b204fb79fbd03024421a9d4ecb
Billy VILLENA <coding@devrendal.fr>
2025-04-03 15:01:19 +0200

    ✨ [Index] Affichage articles publiés

--- src/Controller/Index/IndexGetController.php
+++ src/Controller/Index/IndexGetController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller\Index;
 
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,13 +19,17 @@ use Symfony\Component\Routing\Attribute\Route;
 class IndexGetController extends AbstractController
 {
     public function __invoke(
+        PostRepository $postRepository,
         #[MapQueryParameter]
         ?string $nom = null,
     ): Response {
+        $allPublishedPosts = $postRepository->getAllPublished();
+
         return $this->render(
             view: 'pages/index/index.html.twig',
             parameters: [
                 'nom' => $nom,
+                'published_posts' => $allPublishedPosts,
             ],
         );
     }
